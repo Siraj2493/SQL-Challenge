4ceef0d1ac6dd44d60404cc02568d098ba31267c
Siraj2493 <pirzadasir@gmail.com>
2020-03-10 21:15:45 -0400

    Update data_import.sql

--- EmployeeSQL/data_import.sql
+++ EmployeeSQL/data_import.sql
@@ -1,8 +1,8 @@
-COPY "department"
+COPY "departments"
 FROM 'C:\data_import\departments.csv' 
 DELIMITER ',' CSV HEADER;
 
-COPY "employee"
+COPY "employees"
 FROM 'C:\data_import\employees.csv'
 DELIMITER ',' CSV HEADER;
 
@@ -10,14 +10,14 @@ COPY "dept_emp"
 FROM 'C:\data_import\dept_emp.csv'
 DELIMITER ',' CSV HEADER;
 
-COPY "dept_mgr"
+COPY "dept_manager"
 FROM 'C:\data_import\dept_manager.csv'
 DELIMITER ',' CSV HEADER;
 
-COPY "salary"
+COPY "salaries"
 FROM 'C:\data_import\salaries.csv'
 DELIMITER ',' CSV HEADER;
 
-COPY "title"
+COPY "titles"
 FROM 'C:\data_import\titles.csv'
 DELIMITER ',' CSV HEADER;
